ecd8f4c244d2977b96391f764128101ef7a7b9ed
elijahsp <elijahsanpedro@gmail.com>
2021-02-19 16:18:19 +0800

    test with fix

--- src/components/AccountItem.vue
+++ src/components/AccountItem.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <div class="account-item" >
     <p>
@@ -33,40 +32,4 @@ export default {
     
   }
   
-=======
-<template>
-  <div class="account-item" >
-    <p>
-      
-      {{account.username}}
-      <button @click="$emit('del-account', account.username)" class="del">x</button>
-      </p>
-  </div>
-</template>
-
-<script>
-export default {
-  name: "AccountItem",
-  props: ["account"],
-  methods: {
-    /*markComplete() {
-      this.todo.completed = !this.todo.completed;
-    }*/
-  }
-}
-</script>
-
-
-<style scoped>
-.del {
-    background: #ff0000;
-    color: #fff;
-    border: none;
-    padding: 5px 9px;
-    border-radius: 50%;
-    cursor: pointer;
-    
-  }
-  
->>>>>>> refs/remotes/turnstile-app/master
 </style>
